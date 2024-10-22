dc30f51e4d6a2f79893df768c6b8dba7e49578ea
glamiturianmai <glamiturianmaioha@gmail.com>
2024-10-23 00:38:52 +0300

    first css

--- src/components/ExcursionCard.vue
+++ src/components/ExcursionCard.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="excursion-card">
-    <h3>{{ excursion.title }}</h3>
-    <p><strong>Цена:</strong> {{ excursion.netto_price }}</p>
-    <p><strong>Рейтинг:</strong> {{ excursion.customers_review_rating }}</p>
-    <p><strong>Кол-во оценок:</strong> {{ excursion.reviews }}</p>
-   <img :src="excursion.main_photo.original" alt="img" class="excursion-image" />
+    <img :src="excursion.main_photo.original" alt="img" class="excursion-image" />
+    <div class="rank-container">
+        <img :src={star} alt="img" class="star-image" />
+        <p class="rank"> {{ excursion.customers_review_rating }}</p>
+        <p class="inf">  {{ excursion.reviews }}</p>
+    </div>
+    <div class="name">{{ excursion.title }}</div>
+    <div class="price-container">
+        <p class="price"> от {{ excursion.netto_price }} Р</p>
+        <p class="inf"> за экскурсию</p>
+    </div>
+   
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-
+import {star} from "./../assets/star.png"
 export default defineComponent({
   props: {
     excursion: {
@@ -34,12 +41,52 @@ export default defineComponent({
 
 <style scoped>
 .excursion-card {
-  
+    display: flex;
+    flex-direction: column;
+    height: 332px;
+    width: 345px;
+    align-items: baseline;
 }
 
 .excursion-image {
-  max-width: 100px; 
-  min-height: 100px; 
+    height: 200px;
+    width: 345px;
+
  
 }
+
+.rank-container{
+    display: flex;
+    flex-direction: row;
+    gap: 3px;
+}
+.rank{
+    font-size: 14px;
+}
+.inf{
+    font-size: 12px;
+    color: #999999;
+
+}
+
+.name{
+    font-size: 16px;
+    font-weight: 700;
+    color: #23262F;
+
+}
+
+.price-container{
+    display: flex;
+    flex-direction: column;
+    gap: 3px;
+}
+.price{
+    font-size: 22px;
+    font-weight: 700;
+    color: #444444;
+
+}
+
+
 </style>
